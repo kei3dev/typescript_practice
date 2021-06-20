5d45a3a575e0d87d370c13b10a0f18e8824d2d7b
keisuke-333 <keikubo.999@gmail.com>
2021-06-21 00:03:13 +0900

    レストパラメーターを持つ関数の型定義

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { logMessage } from './function/basic'
-import { isUserSignedIn, isUserSignedIn2 } from './function/parameters'
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
 
 logMessage('test')
 isUserSignedIn('ABC', 'hoge')
 isUserSignedIn('DEF', 'fuga')
 isUserSignedIn('GHI')
 isUserSignedIn2('ABC')
+const sum = sumProductsPrice(100, 200, 300, 400, 500, 600)
+console.log('レストパラメーターを持つ関数', sum)
