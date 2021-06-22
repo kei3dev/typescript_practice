37b914980e78268a43f87cd14917f2660ee27099
keisuke-333 <keikubo.999@gmail.com>
2021-06-23 00:28:38 +0900

    合併型(union)と交差型(intersection)

--- src/object/alias.ts
+++ src/object/alias.ts
@@ -1,4 +1,5 @@
 export default function typeAliasSample() {
+  // 型エイリアス
   type Country = {
     capital: string
     language: string
@@ -20,4 +21,55 @@ export default function typeAliasSample() {
   }
 
   console.log(america)
+
+  // 合併型(union)と交差型(intersection)
+  type Knight = {
+    hp: number
+    sp: number
+    weapon: string
+    swordSkill: string
+  }
+
+  type Wizard = {
+    hp: number
+    mp: number
+    weapon: string
+    magicSkill: string
+  }
+
+  // 合併型 → KnightまたはWizardの型を持つ
+  type Adventurer = Knight | Wizard
+
+  // 交差型 → KnightかつWizardの型を持つ
+  type Paladin = Knight & Wizard
+
+  // Knight寄りの冒険者
+  const adventurer1: Adventurer = {
+    hp: 100,
+    sp: 30,
+    weapon: '木の剣',
+    swordSkill: '三連斬り',
+  }
+
+  // Wizard寄りの冒険者
+  const adventurer2: Adventurer = {
+    hp: 100,
+    mp: 30,
+    weapon: '木の杖',
+    magicSkill: 'ファイヤボール',
+  }
+
+  console.log(adventurer1)
+  console.log(adventurer2)
+
+  const paladin: Paladin = {
+    hp: 300,
+    sp: 100,
+    mp: 100,
+    weapon: '銀の剣',
+    swordSkill: '三連斬り',
+    magicSkill: 'ファイヤボール',
+  }
+
+  console.log(paladin)
 }
