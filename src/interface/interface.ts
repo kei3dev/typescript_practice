2836b9de42f0d34215197a1289a14d793c29528c
keisuke-333 <keikubo.999@gmail.com>
2021-06-27 18:23:46 +0900

    Interfaceと型エイリアスのマージの違い

---
+++ src/interface/interface.ts
@@ -0,0 +1,33 @@
+// —————————————————————————
+// Interfaceで表現
+// —————————————————————————
+
+interface Bread {
+  calories: number
+}
+interface Bread {
+  type: string
+}
+const francePan: Bread = {
+  calories: 300,
+  type: 'hard',
+}
+
+// —————————————————————————
+// 型エイリアスで表現
+// —————————————————————————
+
+type MaboDofu = {
+  calories: number
+  spicyLevel: number
+}
+type Rice = {
+  calories: number
+  gram: number
+}
+type MaboDon = MaboDofu & Rice // 交差型(union)
+const mabodon: MaboDon = {
+  calories: 500,
+  spicyLevel: 10,
+  gram: 350,
+}
