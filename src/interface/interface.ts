55f8ecd93fa5745d6d447c514b93b9fc446b8abc
keisuke-333 <keikubo.999@gmail.com>
2021-06-27 18:38:42 +0900

    インターフェースで継承

--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -31,3 +31,37 @@ const mabodon: MaboDon = {
   spicyLevel: 10,
   gram: 350,
 }
+
+// —————————————————————————
+// インターフェースの継承
+// —————————————————————————
+
+interface Book {
+  page: number
+  title: string
+}
+interface Magazine extends Book {
+  cycle: 'daily' | 'weekly' | 'monthly' | 'yearly'
+}
+const jump: Magazine = {
+  page: 300,
+  title: '週刊少年ジャンプ',
+  cycle: 'weekly',
+}
+
+// —————————————————————————
+// インターフェースで型エイリアスを継承
+// —————————————————————————
+
+type BookType = {
+  page: number
+  title: string
+}
+interface Handbook extends BookType {
+  theme: string
+}
+const cotrip: Handbook = {
+  page: 120,
+  title: 'ことりっぷ',
+  theme: '旅行',
+}
