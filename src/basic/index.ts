6942a07bb17fab36b05930f0829adf2f05d25c0e
keisuke-333 <keikubo.999@gmail.com>
2022-03-31 19:11:34 +0900

    コントローラーの作成まで

--- src/basic/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as unknownSample } from './unknown'
-export { default as anySample } from './any'
-export { default as notExistSample } from './notExist'
-export { default as primitiveSample } from './primitive'
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-import asyncAwaitSample from './asynchronous/asyncAwait'
-asyncAwaitSample()
