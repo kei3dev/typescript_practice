fc9eeabfa3e721600941b3157a4ac31f0a92d25e
keisuke-333 <keikubo.999@gmail.com>
2022-04-02 23:16:39 +0900

    全件取得

--- backend/src/items/items.controller.ts
+++ backend/src/items/items.controller.ts
@@ -7,7 +7,7 @@ export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
   @Get()
-  findAll() {
+  findAll(): Item[] {
     return this.itemsService.findAll()
   }
 
