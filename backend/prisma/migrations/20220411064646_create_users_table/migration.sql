d9adc3edecd15d7dd0ea7b5ecf841590273bb029
keisuke-333 <keikubo.999@gmail.com>
2022-04-11 16:41:19 +0900

    usersテーブル作成

---
+++ backend/prisma/migrations/20220411064646_create_users_table/migration.sql
@@ -0,0 +1,16 @@
+-- AlterTable
+ALTER TABLE `items` MODIFY `created_at` TIMESTAMP(0) NOT NULL DEFAULT NOW(),
+    MODIFY `updated_at` TIMESTAMP(0) NOT NULL DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP;
+
+-- CreateTable
+CREATE TABLE `users` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `status` ENUM('FREE', 'PREMIUM') NOT NULL,
+    `created_at` TIMESTAMP(0) NOT NULL DEFAULT NOW(),
+    `updated_at` TIMESTAMP(0) NOT NULL DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP,
+
+    UNIQUE INDEX `users_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
