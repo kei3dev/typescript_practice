d4a0cf1b8305952baea3a4621f27f0c12d33c077
keisuke-333 <keikubo.999@gmail.com>
2022-04-11 18:31:10 +0900

    ユーザー作成機能の実装

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common'
+import { Prisma, User } from '@prisma/client'
+import { PrismaService } from 'src/prisma.service'
+import { CreateUserDto } from './dto/create-user.dto'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const { name, password, status } = createUserDto
+    const data: Prisma.UserCreateInput = {
+      name,
+      password,
+      status,
+    }
+    return this.prisma.user.create({ data })
+  }
+}
