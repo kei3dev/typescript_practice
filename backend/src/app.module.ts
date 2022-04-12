58bba3293859ab81f08f3583437f462c457198a5
keisuke-333 <keikubo.999@gmail.com>
2022-04-12 18:54:38 +0900

    JWTモジュールの設定

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { ItemsModule } from './items/items.module'
 import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [ItemsModule, AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ItemsModule, AuthModule],
   controllers: [],
   providers: [],
 })
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 import { PrismaService } from 'src/prisma.service'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('JWT_SECRET_KEY'),
+          signOptions: {
+            expiresIn: 3600,
+          },
+        }
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, PrismaService],
 })
