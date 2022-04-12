368d7a07f898581d1b34ac28498d8771c4ccdc09
keisuke-333 <keikubo.999@gmail.com>
2022-04-12 12:08:28 +0900

    パスワードのハッシュ化

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { Prisma, User } from '@prisma/client'
+import * as bcrypt from 'bcrypt'
 import { PrismaService } from 'src/prisma.service'
 import { CreateUserDto } from './dto/create-user.dto'
 
@@ -9,9 +10,11 @@ export class AuthService {
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const { name, password, status } = createUserDto
+    const salt = await bcrypt.genSalt()
+    const hashPassword = await bcrypt.hash(password, salt)
     const data: Prisma.UserCreateInput = {
       name,
-      password,
+      password: hashPassword,
       status,
     }
     return this.prisma.user.create({ data })
