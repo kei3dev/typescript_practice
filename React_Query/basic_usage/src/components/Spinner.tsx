f21e822d88efca5536f3cb89c91d7f5a0087627c
keisuke-333 <keikubo.999@gmail.com>
2022-09-08 02:55:57 +0900

    fix Spinner style

--- React_Query/basic_usage/src/components/Spinner.tsx
+++ React_Query/basic_usage/src/components/Spinner.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 
 const Spinner: FC = () => {
   return (
-    <div className="my-5 h-8 w-8 animate-spin rounded-full border-2 border-pink-600 border-t-transparent"></div>
+    <div className="h-12 w-12 animate-spin rounded-full border-2 border-pink-600 border-t-transparent"></div>
   )
 }
 
