ef360c6ee65165d4e9c68c1f1895ee9e73cfcf90
keisuke-333 <keikubo.999@gmail.com>
2022-10-02 23:35:56 +0900

    add sleep func

--- React_Query/basic_usage/src/hooks/useQueryPosts.ts
+++ React_Query/basic_usage/src/hooks/useQueryPosts.ts
@@ -3,12 +3,18 @@ import { useQuery } from '@tanstack/react-query'
 
 import type { Post } from '@/types'
 
+const sleep = (ms: number): Promise<any> => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
 export const useQueryPosts = () => {
   const getPosts = async () => {
-    const { data } = await axios.get<Post[]>(
-      'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0'
-    )
+    const { data } = await axios
+      .get<Post[]>('https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0')
+      .then(await sleep(5000))
     return data
   }
-  return useQuery<Post[], Error>(['posts'], getPosts)
+  return useQuery<Post[]>(['posts'], getPosts)
 }
