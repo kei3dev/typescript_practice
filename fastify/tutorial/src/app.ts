12cc4b078184d7ef211250ecfda71e62a2f35c4c
keisuke-333 <keikubo.999@gmail.com>
2023-03-04 12:20:47 +0900

    test server

--- fastify/tutorial/src/app.ts
+++ fastify/tutorial/src/app.ts
@@ -1,1 +1,19 @@
-console.log('test')
+import fastify from 'fastify'
+
+const server = fastify()
+
+server.get('/', async (req, res) => {
+  return { status: 'ok' }
+})
+
+const main = async () => {
+  try {
+    await server.listen({ port: 3000, host: '0.0.0.0' })
+    console.log('Server ready at http://localhost:3000')
+  } catch (e) {
+    console.error(e)
+    process.exit(1)
+  }
+}
+
+main()
