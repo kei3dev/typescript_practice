a86ab17d6a4ba004491714d3be265f16399e5e4f
keisuke-333 <keikubo.999@gmail.com>
2023-03-08 12:57:11 +0900

    create user router

---
+++ fastify/tutorial/src/modules/user/user.route.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from 'fastify'
+
+import { registerUserHandler } from './user.controller'
+
+const userRoutes = async (server: FastifyInstance) => {
+  server.post('/', registerUserHandler)
+}
+
+export default userRoutes
