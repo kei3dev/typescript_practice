1c7e17546c47e1e57840f4cd6a4313ec1fcd9e77
keisuke-333 <keikubo.999@gmail.com>
2023-03-23 00:02:05 +0900

    create loginHandler

--- fastify/tutorial/src/modules/user/user.controller.ts
+++ fastify/tutorial/src/modules/user/user.controller.ts
@@ -1,6 +1,9 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { CreateUserInput } from './user.schema'
-import { createUser } from './user.service'
+
+import { server } from '../../app'
+import { verifyPassword } from '../../utils/hash'
+import { CreateUserInput, LoginInput } from './user.schema'
+import { createUser, findUserByEmail } from './user.service'
 
 export const registerUserHandler = async (
   request: FastifyRequest<{ Body: CreateUserInput }>,
@@ -15,4 +18,32 @@ export const registerUserHandler = async (
     console.log(e)
     return reply.code(500).send(e)
   }
+}
+
+export const loginHandler = async (
+  request: FastifyRequest<{ Body: LoginInput }>,
+  reply: FastifyReply
+) => {
+  const body = request.body
+
+  const user = await findUserByEmail(body.email)
+  if (!user) {
+    return reply.code(401).send({
+      message: 'Invalid email or password',
+    })
+  }
+
+  const correctPassword = verifyPassword({
+    candidatePassword: body.password,
+    salt: user.salt,
+    hash: user.password,
+  })
+  if (correctPassword) {
+    const { password, salt, ...rest } = user
+    return { accessToken: server.jwt.sign(rest) }
+  }
+
+  return reply.code(401).send({
+    message: 'Invalid email or password',
+  })
 }
